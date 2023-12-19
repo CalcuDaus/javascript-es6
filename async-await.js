cbc022a376cefd35d909829e1d93f437c97be7fb
daus <dausg878@gmail.com>
2023-12-19 11:30:17 +0700

    async

---
+++ async-await.js
@@ -0,0 +1,27 @@
+// contoh penggunaan async dan await
+const makeCoffe = () => {
+  return new Promise((resolve, reject) => {
+    const coffe = 100;
+
+    if (coffe >= 10) {
+      resolve("Kopi Berhasil didapatkan !");
+    } else {
+      reject("Kopi Gagal didapatkan");
+    }
+  });
+};
+
+// contoh penggunaan biasa promise
+
+const getCoffe = () => {
+  makeCoffe()
+    .then((coffe) => {
+      console.log(coffe);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+
+getCoffe();
